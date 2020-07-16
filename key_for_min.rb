6cd53806fff9cfafe17daf92063798a2a4b9712f
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-16 03:59:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
+  array_a = []
+  array_b = []
+  
   if name_hash.any?
     name_hash.each do |a, b|
-      
+      array_a = a
   else
     NIL
   end
