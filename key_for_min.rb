e625d252314a00e174297684249ca8cd5ac34fb7
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-16 03:57:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  is name_
+  if name_hash.any?
+    name_hash.each do |a, b|
+      
+  else
+    NIL
+  end
 end
